8f684d7a70db5c6f506d9023edf9ef6fefb60f25
Brett Cassette <brett.shollenberger@gmail.com>
2015-03-14 16:08:25 -0700

    Update int-size

--- 1-int-size/int_size.c
+++ 1-int-size/int_size.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <limits.h>
 
+// Int size depends on the machine architecture
+//
+// A 16-bit machine is one in which the length of each word
+// is 16-bits; for signed integers, that means a range of
+// -32768 to 32767
+//
+// A 32-bit machine has a range of -2147483648 to 2147483647
+//
 int main() {
   int min, max;
 
