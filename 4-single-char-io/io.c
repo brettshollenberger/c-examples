6fed33f9edc0251fce7c52884983ad6c83b0700c
Brett Cassette <brett.shollenberger@gmail.com>
2015-03-14 17:21:28 -0700

    Add comments to single char io

--- 4-single-char-io/io.c
+++ 4-single-char-io/io.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+// Since all representations are stored internally as just byte patterns,
+// we're not obligated to store a character as a character.
+//
+// In fact, since we're attempting to check for the char == EOF, a symbolic
+// constant defined in stdio.h (often as -1), we have to ensure that the
+// value of c is large enough to store either a character or whatever
+// integer EOF is defined as.
+//
+// Therefore, we use int type to hold it.
 int main() {
   int c;
 
