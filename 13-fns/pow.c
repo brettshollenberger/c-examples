c9c134db834d14751c817d0fc869f0384eff2470
Brett Cassette <brett.shollenberger@gmail.com>
2015-03-16 20:17:29 -0700

    Add fns

---
+++ 13-fns/pow.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int isEven(int n) {
+  return n % 2 == 0;
+}
+
+int powa(int n, int m) {
+  int i, base;
+  base = n;
+
+  if (isEven(m) != TRUE) {
+    return n * powa(n, m-1);
+  } else {
+    for (i = 1; i < m; i++) {
+      n = n * base;
+    }
+    return n;
+  }
+}
+
+int main() {
+  printf("pow 2, 5 %d", powa(2, 5));
+}
